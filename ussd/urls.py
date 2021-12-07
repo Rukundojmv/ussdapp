e2f870a73411e81605d8f7830f942a7e3b22fdba
unknown <rukundojamie@gmail.com>
2021-12-07 16:34:40 +0200

    changes

--- ussd/urls.py
+++ ussd/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from . import views
 from .views import *
 urlpatterns = [
-    path('', views.welcome, name='welcome') ,
+    # path('', views.welcome, name='welcome') ,
     path('ussd/', views.ussdApp, name='ussd')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
  
