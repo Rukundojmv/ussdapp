7383ce2e7250021e7f566623ec825dd20fbbe6b2
unknown <rukundojamie@gmail.com>
2021-12-11 20:03:40 +0200

    adds updates

--- ussd/views.py
+++ ussd/views.py
@@ -25,8 +25,8 @@ def ussdApp(request):
         if text == '':
             response =  "CON Ikaze kuri chiafarm \n\n"
             response += "1. Kugura igihingwa cya chia \n"
-            response += "2. Iby'ibanze ukwiye kumenya ku gihingwa "
-            response += " cya chia seeds mbere y'uko ukigura"
+            response += "2. Iby'ibanze ukwiye kumenya kuri "
+            response += " chia seeds mbere y'uko uzigura"
 
 
         elif text == '1':
@@ -71,30 +71,30 @@ def ussdApp(request):
             response += "4. Intungamubiri ziboneka mu mbuto za chia \n"
 
 
-        # elif text == '2*1':
-        #     response = "CON Igihingwa cya chia seed ni imbuto zifite ibara ry'umweru cq ry'umukara zikomoka"
-        #     response += "muri Amarica y'amajyepfo ho muri Mexico na Quatemara. Urubuto rumwe rwa chia seed"
-        #     response += "rupima umurambararo wa milimetero imwe (1). Ni ubwoko bw'imbuto z'ibinyampeke."
-        #     response += "Izi mbuto iyo uzishyize mu mazi, rumwe rukurura amazi akubye incuro 12 uburemere "
-        #     response += "bwarwo, maze rukabyimba. Izi mbuto ni igihingwa kerera amezi atatu gusa kandi "
-        #     response += "ikiro kimwe cyazo kigura nibura amafaranga y'u Rwanda 3000."
-        #     response += "Izi mbuto zikaba zarageze mu Rwanda mu mwaka wa 2017."
+        elif text == '2*1':
+            response = "CON Igihingwa cya chia seed ni imbuto zifite ibara ry'umweru cq ry'umukara zikomoka"
+            response += "muri Amarica y'amajyepfo ho muri Mexico na Quatemara. Urubuto rumwe rwa chia seed"
+            response += "rupima umurambararo wa milimetero imwe (1). Ni ubwoko bw'imbuto z'ibinyampeke."
+            response += "Izi mbuto iyo uzishyize mu mazi, rumwe rukurura amazi akubye incuro 12 uburemere "
+            response += "bwarwo, maze rukabyimba. Izi mbuto ni igihingwa kerera amezi atatu gusa kandi "
+            response += "ikiro kimwe cyazo kigura nibura amafaranga y'u Rwanda 3000."
+            response += "Izi mbuto zikaba zarageze mu Rwanda mu mwaka wa 2017."
 
 
-        # elif text == '2*2':
-        #     response = "CON ibiro 2 by'izi mbuto bitera hegitari y'ubutaka maze mu mezi atatu zikaba "
-        #     response += "zeze ibiro igihumbi! Ikindi kiza kurushaho ni uko igihingwa cya chia seed "
-        #     response += "gihingwa hadakoreshejwe ifumbire mva ruganda cyangwa se imiti yica udukoko."
-        #     response += "Uburyo zihingwa, barazitera bagashyiramo ifumbire y'imborera bakazuhira "
-        #     response += "bihagije ubundi zikera."
+        elif text == '2*2':
+            response = "CON ibiro 2 by'izi mbuto bitera hegitari y'ubutaka maze mu mezi atatu zikaba "
+            response += "zeze ibiro igihumbi! Ikindi kiza kurushaho ni uko igihingwa cya chia seed "
+            response += "gihingwa hadakoreshejwe ifumbire mva ruganda cyangwa se imiti yica udukoko."
+            response += "Uburyo zihingwa, barazitera bagashyiramo ifumbire y'imborera bakazuhira "
+            response += "bihagije ubundi zikera."
 
 
-        # elif text == '2*3':
-        #     response = "CON Imbuto za chia seeds zishobora kuribwa uko zakabaye zivanywe mu murima, "
-        #     response += "zishobora gusebwamo ifu cyangwa se zigakorwamo amavuta yaba ayo kurya"
-        #     response += " cyangwa se ayo kwisiga. Mu gaciro, ikiro kimwe cy'igihingwa cya chia seeds "
-        #     response += "kinganya agaciro mu mafaranga n'ibiro 15 by'ikawa y'ibitumbwe yereye amezi 12, "
-        #     response += " kingana n'ibiro 20 by'ibijumba, kingana n'ibiro 5 by'ibishyimbo uramutse ubaze "
-        #     response += "ku mafaranga 400 kuri buri kiro. ikiro cya chia seeds kandi kinganya amafaranga "
-        #     response += "n'ibiro 3 by'umuceri cyangwa se ibiro 7 by'ubugari."
+        elif text == '2*3':
+            response = "CON Imbuto za chia seeds zishobora kuribwa uko zakabaye zivanywe mu murima, "
+            response += "zishobora gusebwamo ifu cyangwa se zigakorwamo amavuta yaba ayo kurya"
+            response += " cyangwa se ayo kwisiga. Mu gaciro, ikiro kimwe cy'igihingwa cya chia seeds "
+            response += "kinganya agaciro mu mafaranga n'ibiro 15 by'ikawa y'ibitumbwe yereye amezi 12, "
+            response += " kingana n'ibiro 20 by'ibijumba, kingana n'ibiro 5 by'ibishyimbo uramutse ubaze "
+            response += "ku mafaranga 400 kuri buri kiro. ikiro cya chia seeds kandi kinganya amafaranga "
+            response += "n'ibiro 3 by'umuceri cyangwa se ibiro 7 by'ubugari."
         return HttpResponse(response)
