7980ec9a01a9f30d3db57c951bb0b5b6ac1366bb
unknown <rukundojamie@gmail.com>
2021-12-14 14:28:01 +0200

    addons

--- ussd/iteganya.py
+++
@@ -1,9 +0,0 @@
-from .models import * 
-
-def insertData(category, sessionID, phoneNumber):
-    insert = Iteganyagihe(category=category, 
-     sessionId=sessionID,
-     phonNumber=phoneNumber
-    )
-    saveDate= insert.save()
-    return saveDate
